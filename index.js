aecf59c119db8dc0a31b18a2e10aeaa2368594a7
Eli Sacks <bastlifa@gmail.com>
2019-09-04 11:40:48 -0400

    middleware for users and logger added

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // code away!
+const express = require('express')
+const userRouter = require('./users/userRouter')
+
+
+const server = express()
+server.use(express.json())
+server.use('/api/users', userRouter)
+
+function logger(req, res, next) {
+    console.log(
+        `[${new Date().toISOString()}] ${req.method} to ${req.url}`
+    )
+    next()
+}
+
+
+
+
+
+server.use(logger)
+server.use(validateUserID)
+
+const server = express()
+
+server.listen(5000, _ =>
+{
+    console.log(`Server listening on port ${5000}`)
+})
--- users/userRouter.js
+++ users/userRouter.js
@@ -1,5 +1,5 @@
 const express = 'express';
-
+const userDB = require('./userDb')
 const router = express.Router();
 
 router.post('/', (req, res) => {
@@ -32,16 +32,24 @@ router.put('/:id', (req, res) => {
 
 //custom middleware
 
-function validateUserId(req, res, next) {
+const validateUserID = (req, res, next) =>
+{
+    const id = req.id
+    if(userDB.getById(Number(id))) next()
+    else res.status(400).json({ message: 'Invalid user id' })
+}
 
-};
-
-function validateUser(req, res, next) {
-
-};
+const validateUser = (req, res, next) =>
+{
+    if(!req.body) res.status(400).json({ message: "missing user data" })
+    else if(!req.body.name) res.status(400).json({ message: "missing required name field" })
+    next()
+}
 
 function validatePost(req, res, next) {
-
+    if(!req.body) res.status(400).json({ message: "missing post data" })
+    else if(!req.body.text) res.status(400).json({ message: "missing required text field" })
+    next()
 };
 
 module.exports = router;
